cfa29ff80dd6428e4b1b3b0184ca26cc4b100ba4
Zach Reedy <paraton@hotmail.com>
2013-08-06 20:00:26 -0400

    Google command

---
+++ Source/Commands/Lucky.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+
+namespace Assbot.Commands
+{
+	public class Lucky : Command
+	{
+		public override string Prefix
+		{
+			get
+			{
+				return "lucky";
+			}
+		}
+
+		public Lucky(Bot parent)
+			: base(parent)
+		{
+
+		}
+
+		public override void HandleDirect(List<string> args, string username)
+		{
+			if (args.Count < 1)
+			{
+				Parent.SendChannelMessage("!lucky <query>");
+				return;
+			}
+
+			Thread thread = new Thread(() =>
+			{
+				string query = Uri.EscapeUriString(String.Join(" ", args));
+				string uri = String.Format("http://www.google.com/search?q={0}&btnI", query);
+
+				HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+
+				Parent.SendChannelMessage(request.GetResponse().ResponseUri.AbsoluteUri);
+			});
+
+			thread.Start();
+
+			base.HandleDirect(args, username);
+		}
+	}
+}
