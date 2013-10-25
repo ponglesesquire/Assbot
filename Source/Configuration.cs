28bedb1c87a95d5756aa4447fe7b656c2d8edc53
Zach Reedy <paraton@hotmail.com>
2013-10-25 15:37:45 -0400

    Some more fixes
    Also small commit so Rawrity can sync properly on his fork.

--- Source/Configuration.cs
+++ Source/Configuration.cs
@@ -3,7 +3,7 @@
 	public static class Configuration
 	{
 		public static string Server = "irc.boredicons.com";
-		public static string Username = "Assbot";
+		public static string Username = "Assbot-dev";
 		public static string Password = "thestairs";
 		public static string Channel = "#gmc";
 		public static char CommandDelimiter = '!';
